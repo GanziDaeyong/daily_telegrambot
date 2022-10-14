4a68fe1cc2f40c54acbd61b15293d404ef48abff
ganzidaeyong <iwin1203@naver.com>
2022-10-14 14:48:17 +0000

    basic send logic

--- handler.py
+++ handler.py
@@ -1,10 +1,28 @@
-import json
+# import json
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
+# def hello(event, context):
+#     body = {
+#         "message": "Go Serverless v3.0! Your function executed successfully!",
+#         "input": event,
+#     }
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+#     return {"statusCode": 200, "body": json.dumps(body)}
+
+import asyncio
+import telegram
+import os
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+
+async def sendmsg() -> None:
+    bot = telegram.Bot(BOT_TOKEN)
+    await bot.send_message(CHAT_ID, text="TESTING...")
+
+
+asyncio.run(sendmsg())
+
+
+# main()
