1a3876015dcbab7405f9b5a8dee025c5029da9b1
ganzidaeyong <iwin1203@naver.com>
2022-10-15 14:50:25 +0000

    ssm

--- handler.py
+++ handler.py
@@ -1,28 +1,26 @@
-# import json
-
-
-# def hello(event, context):
-#     body = {
-#         "message": "Go Serverless v3.0! Your function executed successfully!",
-#         "input": event,
-#     }
-
-#     return {"statusCode": 200, "body": json.dumps(body)}
-
+import json
 import asyncio
 import telegram
 import os
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
-
 
 async def sendmsg() -> None:
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    CHAT_ID = os.environ.get("CHAT_ID")
     bot = telegram.Bot(BOT_TOKEN)
-    await bot.send_message(CHAT_ID, text="TESTING...")
+    # await bot.send_message(CHAT_ID, text="TESTING...")
+    bot.send_message(CHAT_ID, text="TESTING...")
 
 
-asyncio.run(sendmsg())
+def noti(event, context):
 
+    asyncio.run(sendmsg())
 
-# main()
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+        # "mysecret": process.env.
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
+
